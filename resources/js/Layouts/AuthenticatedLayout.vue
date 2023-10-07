1443310980221e12be3a43db47f3957daf0e1ac3
Vivian Hayes <vivian.xw7@gmail.com>
2023-10-07 16:43:41 -0500

    update nav menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('chirps.index')" :active="route().current('chirps.index')">
+                                    Chirps
+                                </NavLink>
                             </div>
                         </div>
 
@@ -115,6 +118,9 @@ const showingNavigationDropdown = ref(false);
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
                         </ResponsiveNavLink>
+                        <NavLink :href="route('chirps.index')" :active="route().current('chirps.index')">
+                            Chirps
+                        </NavLink>
                     </div>
 
                     <!-- Responsive Settings Options -->
